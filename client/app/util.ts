864bb2c73a2392fef2d79f93dd8dc326b50d20c8
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2025-01-03 13:24:05 +0200

    Minor cleanup

--- client/app/util.ts
+++ client/app/util.ts
@@ -1,4 +1,4 @@
-const isCraig = false;
+const isCraig = true;
 export const userId = isCraig ? 1 : 2;
 export const userTag = isCraig ? '$craig.turner' : '$susan.allen';
 export const API_ENDPOINT = process.env.API_ENDPOINT || 'http://localhost:3000';
